f5a4e97bb5d83196cae4e4ee3d6cf942427ed257
rayhan9999 <rayhanchowdhuri2023@gmail.com>
2023-03-29 20:08:42 +0600

    show data from cart

--- src/Components/Header/Shop/Shop.jsx
+++ src/Components/Header/Shop/Shop.jsx
@@ -17,6 +17,7 @@ const Shop = () => {
     useEffect(() => {
         const savedCart = [];
         const storedCart = getShoppingCart();
+
         //step 1:get id of the added product 
         for (const id in storedCart) {
             //step 2:get product from products state by using by id
@@ -59,7 +60,23 @@ const Shop = () => {
     // }, [products])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        //cart.push(product)
+        let newCart = [];
+        //const newCart = [...cart, product];
+        //if product does not exist in the cart ,then set quantity =1
+        //if exist update quantity by 1
+
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product]
+        } else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+
+
         setCart(newCart);
         addToDb(product.id)
 
