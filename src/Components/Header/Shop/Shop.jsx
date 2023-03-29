6c2b0a918522d59ebd82c02e6aad3db029ee8f85
rayhan9999 <rayhanchowdhuri2023@gmail.com>
2023-03-29 19:32:21 +0600

    retrive product from stored cart and set it to card

--- src/Components/Header/Shop/Shop.jsx
+++ src/Components/Header/Shop/Shop.jsx
@@ -15,9 +15,48 @@ const Shop = () => {
     }, []);
 
     useEffect(() => {
-        const storedCart = getShoppingCart;
-        console.log(storedCart)
-    }, [])
+        const savedCart = [];
+        const storedCart = getShoppingCart();
+        //step 1:get id of the added product 
+        for (const id in storedCart) {
+            //step 2:get product from products state by using by id
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                //step 3:add quantity 
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                //step 4: add the added product to the saved cart 
+                savedCart.push(addedProduct);
+
+
+
+
+            }
+            //console.log('added product', addedProduct)
+            //step 5:set the cart 
+            setCart(savedCart);
+
+        }
+
+    }, [products])
+
+    // useEffect(() => {
+
+    //     const storedCart = getShoppingCart();
+    //     const savedCart = [];
+    //     //step 1: get id
+    //     for (const id in storedCart) {
+    //         //step 2:get the product by using id:
+    //         const addedProduct = products.find(product =>
+    //             product.id === id)
+    //         console.log(addedProduct)
+    //         //step 3:get quantity of the product 
+    //         const quantity = storedCart[id];
+    //         addedProduct.quantity = quantity;
+    //         console.log(addedProduct)
+
+    //     }
+    // }, [products])
 
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
